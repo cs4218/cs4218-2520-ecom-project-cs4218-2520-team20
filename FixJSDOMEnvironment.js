93a7443b54c1aae044570015e480cdf3da653767
wzwren <115881359+WZWren@users.noreply.github.com>
2026-02-09 02:05:39 +0800

    Add additional config to test environment
    See https://github.com/jsdom/jsdom/issues/3363#issuecomment-1467894943.
    This allows us to use the native deep-copy function introduced in Node 17.

---
+++ FixJSDOMEnvironment.js
@@ -0,0 +1,14 @@
+// Forces Jest environment to use structuredClone introduced in Node 17
+// Credit to tkrotoff for the fix: https://github.com/jsdom/jsdom/issues/3363#issuecomment-1467894943
+
+import JSDOMEnvironment from 'jest-environment-jsdom';
+
+// https://github.com/facebook/jest/blob/v29.4.3/website/versioned_docs/version-29.4/Configuration.md#testenvironment-string
+export default class FixJSDOMEnvironment extends JSDOMEnvironment {
+  constructor(...args) {
+    super(...args);
+
+    // FIXME https://github.com/jsdom/jsdom/issues/3363
+    this.global.structuredClone = structuredClone;
+  }
+}
