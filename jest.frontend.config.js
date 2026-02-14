cee65dffe7f2ef2a9619feae545e62ae17bc8444
wzwren <115881359+WZWren@users.noreply.github.com>
2026-02-14 23:15:06 +0800

    Make format for testMatch friendlier to git changes

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -20,11 +20,17 @@ export default {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js", "<rootDir>/client/src/pages/user/*.test.js"],
+  testMatch: [
+    "<rootDir>/client/src/pages/Auth/*.test.js",
+    "<rootDir>/client/src/pages/user/*.test.js"
+  ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**", "client/src/pages/user/Profile.**"],
+  collectCoverageFrom: [
+    "client/src/pages/Auth/**",
+    "client/src/pages/user/Profile.**"
+  ],
   coverageThreshold: {
     global: {
       lines: 100,
