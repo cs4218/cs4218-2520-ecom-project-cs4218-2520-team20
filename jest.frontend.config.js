080272dc86044098f61e3f1771d39777a81be888
wzwren <115881359+WZWren@users.noreply.github.com>
2026-02-15 23:25:18 +0800

    Adjust frontend config to be more git-friendly

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -20,11 +20,15 @@ export default {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js"],
+  testMatch: [
+    "<rootDir>/client/src/pages/Auth/*.test.js"
+  ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**"],
+  collectCoverageFrom: [
+    "client/src/pages/Auth/**"
+  ],
   coverageThreshold: {
     global: {
       lines: 100,
