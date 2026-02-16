a1665d330cf58f15bfbf1497004d48f182878849
wzwren <115881359+WZWren@users.noreply.github.com>
2026-02-17 02:26:06 +0800

    Fix: Add unique key to every list mapping

--- client/src/pages/user/Orders.js
+++ client/src/pages/user/Orders.js
@@ -31,7 +31,7 @@ const Orders = () => {
             <h1 className="text-center">All Orders</h1>
             {orders?.map((o, i) => {
               return (
-                <div className="border shadow">
+                <div className="border shadow" key={o._id}>
                   <table className="table">
                     <thead>
                       <tr>
@@ -56,7 +56,7 @@ const Orders = () => {
                   </table>
                   <div className="container">
                     {o?.products?.map((p, i) => (
-                      <div className="row mb-2 p-3 card flex-row" key={p._id}>
+                      <div className="row mb-2 p-3 card flex-row" key={`${p._id}_${i}`}>
                         <div className="col-md-4">
                           <img
                             src={`/api/v1/product/product-photo/${p._id}`}
