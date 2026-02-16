d50753cf25abe38e8ccfc6a4c637baf4fd94f6aa
wzwren <115881359+WZWren@users.noreply.github.com>
2026-02-17 02:24:26 +0800

    Add tests for Orders frontend file to config

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -22,12 +22,14 @@ export default {
   // only run these tests
   testMatch: [
     "<rootDir>/client/src/pages/Auth/*.test.js",
+    "<rootDir>/client/src/pages/user/*.test.js",
   ],
 
   // jest code coverage
   collectCoverage: true,
   collectCoverageFrom: [
     "client/src/pages/Auth/**",
+    "client/src/pages/user/Orders.**",
   ],
   coverageThreshold: {
     global: {
