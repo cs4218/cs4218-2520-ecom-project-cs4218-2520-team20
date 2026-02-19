c5a262421c74adb2f812d0b228cfc5001766d497
wzwren <115881359+WZWren@users.noreply.github.com>
2026-02-20 02:29:49 +0800

    Adjust indent for order tests to be 2 spaces

--- models/orderModel.test.js
+++ models/orderModel.test.js
@@ -2,83 +2,83 @@ import mongoose from 'mongoose';
 import OrderModel from './orderModel';
 
 const canned_order = {
-    products: [ new mongoose.Types.ObjectId('000000000000000000000000') ],
-    payment: true,
-    buyer: new mongoose.Types.ObjectId('000000000000000000000001'),
-    status: 'Processing'
+  products: [new mongoose.Types.ObjectId('000000000000000000000000')],
+  payment: true,
+  buyer: new mongoose.Types.ObjectId('000000000000000000000001'),
+  status: 'Processing'
 }
 
 const expected_status = ['Not Processed', 'Processing', 'Shipped', 'Delivered', 'Cancelled']
 
 describe('Order Model', () => {
-    it('should allow an order when supplied with every relevant field', async () => {
-        const order = new OrderModel(canned_order);
+  it('should allow an order when supplied with every relevant field', async () => {
+    const order = new OrderModel(canned_order);
 
-        const validate = order.validate();
+    const validate = order.validate();
 
-        await expect(validate).resolves.not.toThrow();
-    });
+    await expect(validate).resolves.not.toThrow();
+  });
 
-    it('should allow an order even when not supplied a status field', async () => {
-        const modded_order = {
-            ...canned_order,
-        }
-        delete modded_order.status;
-        const order = new OrderModel(canned_order);
+  it('should allow an order even when not supplied a status field', async () => {
+    const modded_order = {
+      ...canned_order,
+    }
+    delete modded_order.status;
+    const order = new OrderModel(canned_order);
 
-        const validate = order.validate();
+    const validate = order.validate();
 
-        await expect(validate).resolves.not.toThrow();
-    });
-    
-    it('should fail when order is not supplied with products', async () => {
-        const modded_order = {
-            ...canned_order,
-            products: [],
-        }
-        modded_order.products = [];
-        const order = new OrderModel(modded_order);
+    await expect(validate).resolves.not.toThrow();
+  });
 
-        const validate = order.validate();
+  it('should fail when order is not supplied with products', async () => {
+    const modded_order = {
+      ...canned_order,
+      products: [],
+    }
+    modded_order.products = [];
+    const order = new OrderModel(modded_order);
 
-        await expect(validate).rejects.toThrow('An order should have at least 1 product.');
-    });
+    const validate = order.validate();
 
-    it('should fail when order is not supplied with a buyer', async () => {
-        const modded_order = {
-            ...canned_order
-        }
-        delete modded_order.buyer;
-        const order = new OrderModel(modded_order);
+    await expect(validate).rejects.toThrow('An order should have at least 1 product.');
+  });
 
-        const validate = order.validate();
+  it('should fail when order is not supplied with a buyer', async () => {
+    const modded_order = {
+      ...canned_order
+    }
+    delete modded_order.buyer;
+    const order = new OrderModel(modded_order);
 
-        await expect(validate).rejects.toThrow('Order validation failed: buyer: Path `buyer` is required.');
-    });
+    const validate = order.validate();
 
-    describe('Status Field Enums', () => {
-        expected_status.forEach(status => it(`should allow "${status}" as a possible status field value`, async () => {
-            const modded_order = {
-                ...canned_order,
-                status: status
-            }
-            const order = new OrderModel(modded_order);
+    await expect(validate).rejects.toThrow('Order validation failed: buyer: Path `buyer` is required.');
+  });
 
-            const validate = order.validate();
+  describe('Status Field Enums', () => {
+    expected_status.forEach(status => it(`should allow "${status}" as a possible status field value`, async () => {
+      const modded_order = {
+        ...canned_order,
+        status: status
+      }
+      const order = new OrderModel(modded_order);
 
-            await expect(validate).resolves.not.toThrow();
-        }))
+      const validate = order.validate();
 
-        it('should not allow values not specified in its enum', async () => {
-            const modded_order = {
-                ...canned_order,
-                status: 'test failure'
-            }
-            const order = new OrderModel(modded_order);
+      await expect(validate).resolves.not.toThrow();
+    }))
 
-            const validate = order.validate();
+    it('should not allow values not specified in its enum', async () => {
+      const modded_order = {
+        ...canned_order,
+        status: 'test failure'
+      }
+      const order = new OrderModel(modded_order);
 
-            await expect(validate).rejects.toThrow('Order validation failed: status: `test failure` is not a valid enum value for path `status`.');
-        })
-    });
+      const validate = order.validate();
+
+      await expect(validate).rejects.toThrow('Order validation failed: status: `test failure` is not a valid enum value for path `status`.');
+    })
+  });
 })
