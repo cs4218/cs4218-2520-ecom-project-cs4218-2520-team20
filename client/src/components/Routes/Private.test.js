27e0b92bbf0d2f7fe4430a6dd99a3d6d83b3cd6b
wzwren <115881359+WZWren@users.noreply.github.com>
2026-02-20 02:19:34 +0800

    Adjust indents for Private and userModel to be 2 spaces

--- client/src/components/Routes/Private.test.js
+++ client/src/components/Routes/Private.test.js
@@ -10,19 +10,19 @@ import PrivateRoute from './Private';
 
 // CANNED RESPONSES
 const canned_auth = {
-    token: 'test_token_value'
+  token: 'test_token_value'
 }
 
 const mock_provider = jest.fn()
 
 // LAYOUT MOCKS
 jest.mock('react-router-dom', () => ({
-    Outlet: jest.fn((props) => (<div>Outlet</div>))
+  Outlet: jest.fn((props) => (<div>Outlet</div>))
 }));
 
 jest.mock('../Spinner', () => ({
-    __esModule: true,
-    default: jest.fn((props) => (<div>Spinner</div>))
+  __esModule: true,
+  default: jest.fn((props) => (<div>Spinner</div>))
 }))
 
 // LIBRARY MOCKS
@@ -32,50 +32,50 @@ jest.mock('axios');
 const mock_auth = jest.spyOn(Auth, 'useAuth')
 
 describe('Private Routes', () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-    it('should successfully render the Outlet element from the library when an auth token exists', async () => {
-        mock_auth.mockReturnValue([canned_auth, mock_provider]);
-        axios.get.mockResolvedValueOnce({ data: { ok: true } });
+  it('should successfully render the Outlet element from the library when an auth token exists', async () => {
+    mock_auth.mockReturnValue([canned_auth, mock_provider]);
+    axios.get.mockResolvedValueOnce({ data: { ok: true } });
 
-        await act(async () => { render(<PrivateRoute/>) });
+    await act(async () => { render(<PrivateRoute />) });
 
-        await waitFor(() => expect(axios.get).toHaveBeenCalled());
-        expect(Outlet).toHaveBeenCalledTimes(1);
-    });
+    await waitFor(() => expect(axios.get).toHaveBeenCalled());
+    expect(Outlet).toHaveBeenCalledTimes(1);
+  });
 
-    it('should not render the Outlet element when axios responds with no OK', async () => {
-        mock_auth.mockReturnValue([canned_auth, mock_provider]);
-        axios.get.mockResolvedValueOnce({ data: { ok: false } });
+  it('should not render the Outlet element when axios responds with no OK', async () => {
+    mock_auth.mockReturnValue([canned_auth, mock_provider]);
+    axios.get.mockResolvedValueOnce({ data: { ok: false } });
 
-        await act(async () => { render(<PrivateRoute/>) });
+    await act(async () => { render(<PrivateRoute />) });
 
-        await waitFor(() => expect(axios.get).toHaveBeenCalled());
-        expect(Outlet).not.toHaveBeenCalled();
-        expect(Spinner).toHaveBeenCalledTimes(1);
-    });
+    await waitFor(() => expect(axios.get).toHaveBeenCalled());
+    expect(Outlet).not.toHaveBeenCalled();
+    expect(Spinner).toHaveBeenCalledTimes(1);
+  });
 
-    it('should not render the Outlet element when an auth token does not exists', async () => {
-        const empty_auth = { token: "" }
-        mock_auth.mockReturnValueOnce([empty_auth, mock_provider]);
+  it('should not render the Outlet element when an auth token does not exists', async () => {
+    const empty_auth = { token: "" }
+    mock_auth.mockReturnValueOnce([empty_auth, mock_provider]);
 
-        await act(async () => { render(<PrivateRoute/>) });
+    await act(async () => { render(<PrivateRoute />) });
 
-        await waitFor(() => expect(axios.get).not.toHaveBeenCalled());
-        expect(Outlet).not.toHaveBeenCalled();
-        expect(Spinner).toHaveBeenCalledTimes(1);
-    });
+    await waitFor(() => expect(axios.get).not.toHaveBeenCalled());
+    expect(Outlet).not.toHaveBeenCalled();
+    expect(Spinner).toHaveBeenCalledTimes(1);
+  });
 
-    it('should gracefully fail and render the Spinner element when axios encounters an error', async () => {
-        mock_auth.mockReturnValueOnce([canned_auth, mock_provider]);
-        axios.get.mockRejectedValueOnce('some network error');
+  it('should gracefully fail and render the Spinner element when axios encounters an error', async () => {
+    mock_auth.mockReturnValueOnce([canned_auth, mock_provider]);
+    axios.get.mockRejectedValueOnce('some network error');
 
-        await act(async () => { render(<PrivateRoute/>) });
+    await act(async () => { render(<PrivateRoute />) });
 
-        await waitFor(() => expect(axios.get).toHaveBeenCalled());
-        expect(Outlet).not.toHaveBeenCalled();
-        expect(Spinner).toHaveBeenCalledTimes(1);
-    });
+    await waitFor(() => expect(axios.get).toHaveBeenCalled());
+    expect(Outlet).not.toHaveBeenCalled();
+    expect(Spinner).toHaveBeenCalledTimes(1);
+  });
 })
