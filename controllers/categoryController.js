2e454477161ffcf077853057fbbcedca2a08c63f
dylkaw <37286651+dylkaw@users.noreply.github.com>
2026-02-22 22:14:38 +0800

    Add slug and success check for singleCat

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -92,7 +92,23 @@ export const categoryController = async (req, res) => {
 // single category
 export const singleCategoryController = async (req, res) => {
   try {
-    const category = await categoryModel.findOne({ slug: req.params.slug });
+    const { slug } = req.params;
+
+    if (!slug) {
+      return res.status(400).send({
+        success: false,
+        message: "Slug is required",
+      });
+    }
+
+    const category = await categoryModel.findOne({ slug });
+
+    if (!category) {
+      return res.status(404).send({
+        success: false,
+        message: "Category not found",
+      });
+    }
     res.status(200).send({
       success: true,
       message: "Get Single Category Successfully",
