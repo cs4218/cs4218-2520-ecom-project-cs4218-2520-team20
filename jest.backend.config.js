5ccc575541e3a430274ecbdf5fcc3f12bb5810e7
Nigel <nigelleexy@gmail.com>
2026-02-22 15:11:22 +0800

    Edit Jest Config to include the running of more tests

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -6,7 +6,7 @@ export default {
   testEnvironment: "node",
 
   // which test to run
-  testMatch: ["<rootDir>/controllers/*.test.js"],
+  testMatch: ["<rootDir>/controllers/*.test.js", "<rootDir>/helpers/*.test.js", "<rootDir>/middlewares/*.test.js"],
 
   // jest code coverage
   collectCoverage: true,
