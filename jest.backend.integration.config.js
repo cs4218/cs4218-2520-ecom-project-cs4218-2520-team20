20fc0da79c8272a58baa5ce17791443f2d32aefc
wzwren <115881359+WZWren@users.noreply.github.com>
2026-03-21 21:32:29 +0800

    Remove example folder and config

--- jest.backend.integration.config.js
+++ jest.backend.integration.config.js
@@ -12,8 +12,6 @@ export default {
 
   // which test to run
   testMatch: [
-   // TODO: Remove at end of MS2 - Early removal will probably cause merge conflicts
-    "<rootDir>/integrations/backend/example/*.test.js",
     "<rootDir>/integrations/backend/**/*.test.js",
   ],
   
