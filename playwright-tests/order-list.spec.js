67982e9dee1c0824652748f6d4edbbc94a7ee929
wzwren <115881359+WZWren@users.noreply.github.com>
2026-03-22 21:49:35 +0800

    Fix braintree issue for error handling - must be >2000$ item
    Note this ended up revealing a separate UI issue where the item isn't added to
    the cart specifically on the ProductDetail page. As this was black-box tested
    I had opted not to fix this bug.

--- playwright-tests/order-list.spec.js
+++ playwright-tests/order-list.spec.js
@@ -1,11 +1,15 @@
 // Wang Zhi Wren, A0255368U
+import "dotenv/config";
 import { test } from "./seed-db"
 import { expect } from "@playwright/test";
+import productModel from "../models/productModel";
+import mongoose from "mongoose";
+import categoryModel from "../models/categoryModel";
 
 const braintree_test_card_mastercard = '5555 5555 5555 4444'
-const braintree_test_card_failure = '5105 1051 0510 5100'
 
 test('Verify orders page updates properly on success', async ({ reset_db, page }) => {
+  test.slow()
   await page.goto('http://localhost:3000/login');
   await page.getByRole('textbox', { name: 'Enter Your Email' }).click();
   await page.getByRole('textbox', { name: 'Enter Your Email' }).fill('user@test.com');
@@ -46,7 +50,23 @@ test('Verify orders page updates properly on success', async ({ reset_db, page }
   await page.getByRole('link', { name: 'Logout' }).click();
 });
 
-test('Verify orders page updates properly with a bad card', async ({ reset_db, page }) => {
+test('Verify orders page updates properly for a failed order', async ({ reset_db, page }) => {
+  test.setTimeout(30000)
+  const ui_mongo_db = `${process.env.MONGO_URL.replace(/\/$/, '')}/ui`;
+  await mongoose.connect(ui_mongo_db);
+  const category = await categoryModel.findOne({ name: 'Seed Category' })
+  const product = await productModel.create({
+    name: "Bad Product",
+    slug: "bad-product",
+    description: "A product made specifically to fail the payment - just buy 1!",
+    price: 2000,
+    category: category._id,
+    quantity: 5,
+    shipping: true,
+  });
+  await product.save();
+  await mongoose.disconnect();
+
   await page.goto('http://localhost:3000/login');
   await page.getByRole('textbox', { name: 'Enter Your Email' }).click();
   await page.getByRole('textbox', { name: 'Enter Your Email' }).fill('user@test.com');
@@ -63,15 +83,14 @@ test('Verify orders page updates properly with a bad card', async ({ reset_db, p
   await expect(page.getByRole('cell', { name: 'Success' })).toBeVisible();
   await expect(page.getByRole('cell', { name: '1' }).nth(1)).toBeVisible();
 
-  await page.getByRole('link', { name: 'Home' }).click();
-  await page.getByRole('button', { name: 'ADD TO CART' }).click();
-  await page.getByRole('button', { name: 'ADD TO CART' }).click();
+  await page.goto('http://localhost:3000/product/bad-product');
+  await expect(page.getByText('Bad Product')).toBeVisible();
   await page.getByRole('button', { name: 'ADD TO CART' }).click();
   await page.getByRole('link', { name: 'Cart' }).click();
   await page.getByRole('button', { name: 'Paying with Card' }).click();
 
   await page.locator('iframe[name="braintree-hosted-field-number"]').contentFrame().getByRole('textbox', { name: 'Credit Card Number' }).click();
-  await page.locator('iframe[name="braintree-hosted-field-number"]').contentFrame().getByRole('textbox', { name: 'Credit Card Number' }).fill(braintree_test_card_failure);
+  await page.locator('iframe[name="braintree-hosted-field-number"]').contentFrame().getByRole('textbox', { name: 'Credit Card Number' }).fill(braintree_test_card_mastercard);
   await page.locator('iframe[name="braintree-hosted-field-expirationDate"]').contentFrame().getByRole('textbox', { name: 'Expiration Date' }).click();
   await page.locator('iframe[name="braintree-hosted-field-expirationDate"]').contentFrame().getByRole('textbox', { name: 'Expiration Date' }).fill('0130');
   await page.locator('iframe[name="braintree-hosted-field-cvv"]').contentFrame().getByRole('textbox', { name: 'CVV' }).click();
@@ -79,9 +98,9 @@ test('Verify orders page updates properly with a bad card', async ({ reset_db, p
   await page.getByRole('button', { name: 'Make Payment' }).click();
   await expect(page).toHaveURL(/\/dashboard\/user\/orders$/, { timeout: 10_000 });
 
-  await expect(page.getByText('Seed ProductA seeded product for testingPrice : 25Seed ProductA seeded product')).toBeVisible();
-  await expect(page.getByRole('cell', { name: 'Success' }).nth(1)).toBeVisible();
-  await expect(page.getByRole('cell', { name: 'Success' }).first()).not.toBeVisible();
+  await expect(page.locator('div').filter({ hasText: /^Bad ProductA bad product for testingPrice : 2000$/ }).nth(1)).toBeVisible();
+  await expect(page.getByRole('cell', { name: 'Failed' })).toBeVisible();
+  await expect(page.getByRole('cell', { name: 'Success' })).toBeVisible();
 
   await page.getByRole('button', { name: 'Test User' }).click();
   await page.getByRole('link', { name: 'Logout' }).click();
